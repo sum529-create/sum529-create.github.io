66ced9d443f4de0bbfe62dda95f753565baddf35
sum529-create <nosumin29@gmail.com>
2024-09-27 16:53:14 +0900

    #pf - github page 설정

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   runtimeCompiler: true,
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = '나의 Vue 앱';  // 원하는 제목으로 변경합니다.
-        return args;
-      });
-  }
-}
+  publicPath:
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === "production" ? "/sum529-create.github.io/" : "/",
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "노수민 | 프론트엔드 개발자 포트폴리오";
+      return args;
+    });
+  },
+  configureWebpack: {
+    output: {
+      publicPath:
+        // eslint-disable-next-line no-undef
+        process.env.NODE_ENV === "production"
+          ? "/sum529-create.github.io/"
+          : "/",
+    },
+  },
+};
