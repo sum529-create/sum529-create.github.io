04096f73b2326e2409512847dd60bb70e8916f29
sum529-create <nosumin29@gmail.com>
2024-09-28 01:35:12 +0900

    #pf - 빌드시 해당 js파일들을 인식하지 못하는 문제가 발생. 최상위 기준으로 변경

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,11 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   runtimeCompiler: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/sum529-create.github.io/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "노수민 | 프론트엔드 개발자 포트폴리오";
       return args;
     });
-  },
-  configureWebpack: {
-    output: {
-      publicPath:
-        process.env.NODE_ENV === "production"
-          ? "/sum529-create.github.io/"
-          : "/",
-    },
   },
 };
